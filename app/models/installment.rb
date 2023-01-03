f418cd40d8aded38cdab9138dd79ad3ee687b5fc
Bruno Silva <brunorsilva_@hotmail.com>
2023-01-03 13:01:32 -0300

    implement model scope and update associations

--- app/models/installment.rb
+++ app/models/installment.rb
@@ -3,4 +3,9 @@ class Installment < ApplicationRecord
 
   validates :value, :expire_date, presence: true
   validates :value, :numericality => { :greater_than_or_equal_to => 0 }
+
+  scope :by_month,
+        ->(month, year) {
+          where("to_char(expire_date, 'MM') = ? and to_char(expire_date, 'YYYY') = ?", month, year)
+        }
 end
--- app/models/wallet.rb
+++ app/models/wallet.rb
@@ -2,6 +2,7 @@ class Wallet < ApplicationRecord
   belongs_to :user
   has_many :debts, dependent: :destroy
   has_many :credits, dependent: :destroy
+  has_many :installments, through: :debts
 
   validates :credit_card_day, numericality: {
       greater_than_or_equal_to: 1,
