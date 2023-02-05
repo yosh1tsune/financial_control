42d31913de25abdcf8d64cd677ea300a3b3715b4
Bruno Silva <brunorsilva_@hotmail.com>
2023-02-05 14:05:30 -0300

    WIP: credit_card filter

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,15 +2,33 @@ class DashboardController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @installments = installments_by_month
+    @installments = filter
   end
 
   private
+
+  def filter
+    return installments.joins(debt: :credit_card).where(filter_params) if filter_params.to_h.any?
+
+    installments
+  end
+
+  def installments
+    if params[:installments].present?
+      Installment.where(id: params[:installments])
+    else
+      installments_by_month
+    end
+  end
 
   def installments_by_month
     current_user.wallet.installments.by_month(
       params[:month] || Date.current.month.to_s,
       params[:year]  || Date.current.year.to_s
-    )
+    ).where(filter_params)
+  end
+
+  def filter_params
+    params.permit(credit_card: :id)
   end
 end
